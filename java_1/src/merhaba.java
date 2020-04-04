ed8c30b8bfe3c80645b63e2c8d3bad0bbebabc6e
Barış <50153950+bariss48@users.noreply.github.com>
2020-04-04 21:03:33 +0300

    Add files via upload

---
+++ java_1/src/merhaba.java
@@ -0,0 +1,14 @@
+
+public class merhaba {
+
+    public static void main(String[] args) {
+      
+    System.out.println("MERHABA JAVA !!!");
+    System.out.println("MERHABA DÜNYA!!1");     
+    System.out.print("JAVA PROGRAMLAMAYA HOŞGELDİNİZ!!!");
+    
+    
+   
+    }
+    
+}
