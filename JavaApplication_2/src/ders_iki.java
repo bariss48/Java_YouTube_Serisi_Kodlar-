010cef360a93d0ae53b838518a40e3f2c3de90f9
Barış <50153950+bariss48@users.noreply.github.com>
2020-04-04 22:35:15 +0300

    Add files via upload

---
+++ JavaApplication_2/src/ders_iki.java
@@ -0,0 +1,65 @@
+
+public class ders_iki {
+    
+    public static void main(String[] args) {
+        
+     /*
+        INT (TAM SAYILAR), DOUBLE (ONDALIKLI KISMI KAPSIYOR)-64 BIT,  FLOAT (ONDALIKLI KISMI KAPSIYOR)-32 BIT,LONG BUYUK DEĞERLİ SAYILARI TUTMAMIZDA İŞE YARAR
+        
+        SHORT ISE KÜÇÜK SAYILARI TUTMAMIZDA İŞE YARAR,
+        
+        CHAR VE STRING
+        
+        STRING KELİMELERİMİZİ TUTARKEN CHAR'DA HARFLERİMİZİ TUTUYORDU
+        
+        BOOLEAN DEĞİŞKEN TİPİMİZ //  SADECE TRUE VEYA FALSE DÖNÜYOR
+      
+     */   
+        
+     
+     boolean a = false;
+     boolean b = true;
+     int sayi;     
+     double sayi_iki = 990000000;
+     float sayi_uc = 90000000;
+     
+     String sehir = "ISTANBUL";
+     String sehir_iki = "ANKARA";
+     
+     char k = 'K';
+     
+     sayi = 5;  
+     long uzun;
+     short kısa;
+     
+     uzun = 1999999999;
+     kısa = 29999;
+     
+     System.out.println(sayi);   
+        
+     System.out.println("BİZİM SAYIMIZ: "+sayi);
+           
+     System.out.println("BİZİM SAYIMIZ: "+sayi/2);  
+     
+     System.out.println("BİZİM SAYIMIZ: "+sayi_iki/2);  
+         
+     System.out.println("BİZİM SAYIMIZ: "+sayi_uc/2);     
+        
+     System.out.println("Şehrimiz: "+sehir_iki);  
+     
+     System.out.println("Şehrimiz: "+sehir);  
+     
+     System.out.println("karakter değişkenimiz: "+k/2);  
+     
+     System.out.println("LONG SAYIMIZ: "+uzun/2);  
+      
+     System.out.println("SHORT SAYIMIZ:  "+kısa/2);  
+    
+     System.out.println(b);
+     
+    }
+    
+    
+    
+    
+}
