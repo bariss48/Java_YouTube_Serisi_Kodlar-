3882030e3bc14aef3c20eb6ea349384b29fc7507
Barış <50153950+bariss48@users.noreply.github.com>
2020-04-05 15:58:57 +0300

    Add files via upload

---
+++ java_uc/src/opreratorler.java
@@ -0,0 +1,24 @@
+
+public class opreratorler {
+    
+    public static void main(String[] args) {
+        
+      int a = 10;  
+      int b = 2; 
+      
+      // && and VE 
+      // || VEYA OR
+      // ^ XOR  false false ve true true durumunda çalışmadı arkadaşlar T-F F-T DURUMUNDA ÇALIŞIYOR 
+      
+      
+      if( (a == 10) || (b != 5)) {
+          
+          System.out.print("MERHABA");
+          
+          
+      }
+        
+        
+    }
+    
+}
