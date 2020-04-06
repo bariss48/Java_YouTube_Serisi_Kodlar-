466ca1671e05d54401aa70d27f8fb081ac356c08
Barış <50153950+bariss48@users.noreply.github.com>
2020-04-06 23:09:59 +0300

    Add files via upload

---
+++ java_dort(if-else)/src/if_else.java
@@ -0,0 +1,31 @@
+
+public class if_else {
+
+    public static void main(String[] args) {
+      
+     int a,b,c;       
+       
+     a = 10;
+     b = 20;
+     c = 15;
+     
+   if(a>b){
+       
+       if(c>b){
+           
+           System.out.print("İÇERDEKİ İF ÇALIŞIYOR !!!");
+           
+       }else{
+           
+           System.out.print("İÇERDEKİ İF İN ELSE'İ ÇALIŞIYOR");
+           
+       }
+       
+   }else{
+       System.out.print("YUKARIDAKİ İF IN ELSE'İ  ÇALIŞIYOR ");
+       
+   }
+     
+    }
+    
+}
