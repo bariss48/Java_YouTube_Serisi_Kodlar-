14ae9658081c95bdd5d32e206a7ee22824a582b8
Barış <50153950+bariss48@users.noreply.github.com>
2020-04-16 18:41:50 +0300

    Add files via upload

---
+++ for_donguleri_java_altı/src/for_dongusu.java
@@ -0,0 +1,50 @@
+
+public class for_dongusu {
+
+    public static void main(String[] args) {
+        
+        int i;
+        
+        int toplam  = 0;
+        
+        for(i = 1;i<10;i++){
+            
+          toplam += i;
+          
+        }
+        
+        System.out.println(toplam);
+        
+        for(int j=0;j<1000;j++){
+            
+            if(j % 2 == 0){
+            
+            }else{
+                System.out.print(j);
+                System.out.print("-");
+            }
+          
+            
+        }
+        System.out.println();
+        
+       int carpım = 1;
+        
+        for(int k=1;k<20;k++){
+          
+          carpım = carpım * k;
+            
+        }
+        
+        System.out.println(carpım);
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
