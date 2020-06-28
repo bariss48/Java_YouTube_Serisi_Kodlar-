35b679f0619598b43d646f251977fe070fffaac3
Barış <50153950+bariss48@users.noreply.github.com>
2020-06-28 18:39:13 +0300

    Add files via upload

---
+++ alistirmalar/src/Alistirmalar.java
@@ -0,0 +1,86 @@
+
+import java.util.Scanner;
+
+public class Alistirmalar {
+
+   
+    public static void main(String[] args) {
+       
+        for (int i = 0; i < 5; i++) {
+            System.out.println();
+        for (int j = 0; j<i+1; j++) {    
+            System.out.print("*");
+            }  
+        }
+         System.out.println();
+            
+           for (int k = 5; 0 < k; k--) {
+            System.out.println();
+            
+            for (int j = 0; j<k; j++) {
+                
+                System.out.print("*");
+                
+                
+                
+            }
+            
+           
+        }
+           
+            System.out.println();
+          
+          
+           for (int k = 5; 0 < k; k--) {
+            System.out.println();
+        
+            for (int j = 5; 0<j; j--) {
+                
+             if(j <= k){
+                 
+               System.out.print("*");
+                 
+                 
+             }else{
+                 
+               System.out.print(" ");
+                 
+                 
+             }  
+           
+        }
+       }    
+           System.out.println();
+           
+           
+            for (int a = 1; a < 6; a++) {
+            System.out.println();
+        
+            for (int j = 5; 0<j; j--) {
+                
+             if(j <= a){
+                 
+               System.out.print("*");
+                 
+                 
+             }else{
+                 
+               System.out.print(" ");
+                 
+                 
+             }  
+           
+        }
+           
+           
+    }
+
+    }
+ }
+ 
+    
+
+    
+
+    
+
